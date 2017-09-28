b445eee4cbfa25b570b81ebb743a76343c0b0954
Courtney Brousseau <courtney.brousseau@berkeley.edu>
2017-09-28 01:30:52 -0700

    Added "bonus question" functionality

--- js/root.js
+++ js/root.js
@@ -2,8 +2,5 @@ var app = angular.module('root', [])
     .controller('controller', function($scope) {
         $scope.firstName = "John";
         $scope.lastName = "Doe";
-        $scope.isFirstElementVisible = true;
-        $scope.show = function () { 
-        	return $scope.firstName === "Courtney"; 
-        };
+        $scope.isFirstElementVisible = false;
     });
