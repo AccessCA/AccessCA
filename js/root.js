b64f49f8555bb9d3bfe271d050331d970863bf31
Courtney Brousseau <courtney.brousseau@berkeley.edu>
2017-09-28 17:59:14 -0700

    Added SSI and a selection of wants

--- js/root.js
+++ js/root.js
@@ -1,8 +1,14 @@
 var app = angular.module('root', [])
     .controller('controller', function($scope) {
+    	$scope.wantsCalFresh = false;
+    	$scope.wantsMediCal;
+    	$scope.wantsCalWORKS;
         $scope.income;
         $scope.numberInHousehold;
         $scope.isStudent;
-        $scope.isEligible = function () { return $scope.income <= 1915 + 670 * ($scope.numberInHousehold - 1) && !($scope.isStudent == "true"); };
-        $scope.isFirstElementVisible = false;
+        $scope.hasSSI;
+        $scope.numberOnSSI;
+        $scope.isEligible = function () { return $scope.income <= 1915 + 670 * ($scope.numberInHousehold - 1) && !($scope.isStudent == "yes" && $scope.numberInHousehold == 1) && !($scope.numberInHousehold == $scope.numberOnSSI); };
+        $scope.checkSSINumber = function () { return $scope.numberInHousehold > 1 && $scope.hasSSI == "yes"; };
+		$scope.isFirstElementVisible = false;
     });
