aaa59ba6300db42b427cbd6c97ef2e66940efc19
Merric <mdelauney@berkeley.edu>
2017-10-02 16:30:33 -0700

    minor changes

--- js/root.js
+++ js/root.js
@@ -1,16 +1,17 @@
 var app = angular.module('root', [])
     .controller('controller', function($scope) {
     	$scope.wantsCalFresh = false;
-    	$scope.wantsMediCal;
-    	$scope.wantsCalWORKS;
+    	$scope.wantsMediCal = false;
+    	$scope.wantsCalWORKS =false;
         $scope.income;
         $scope.numberInHousehold;
         $scope.isStudent;
         $scope.hasSSI;
         $scope.numberOnSSI;
-        $scope.isEligible = function () { return $scope.income <= 1915 + 670 * ($scope.numberInHousehold - 1) && !($scope.isStudent == "yes" && $scope.numberInHousehold == 1) && !($scope.numberInHousehold == $scope.numberOnSSI) && !($scope.numberInHousehold == 1 && $scope.hasSSI == "yes"); };
-        $scope.showEligible = function () { return $scope.wantsCalFresh && $scope.isEligible(); };
-        $scope.showNotEligible = function () { return $scope.wantsCalFresh && !$scope.isEligible(); };
+        $scope.has60 = false;
+        $scope.hasDisabled = false;
+        $scope.isCalFreshEligible = function () { return $scope.income <= 1915 + 670 * ($scope.numberInHousehold - 1) && !($scope.isStudent == "yes" && $scope.numberInHousehold == 1) && !($scope.numberInHousehold == $scope.numberOnSSI) && !($scope.numberInHousehold == 1 && $scope.hasSSI == "yes"); };
+        $scope.showEligible = function () { return $scope.wantsCalFresh && $scope.isCalFreshEligible(); };
+        $scope.showNotEligible = function () { return $scope.wantsCalFresh && !$scope.isCalFreshEligible(); };
         $scope.checkSSINumber = function () { return $scope.numberInHousehold > 1 && $scope.hasSSI == "yes"; };
-		$scope.isFirstElementVisible = false;
     });
