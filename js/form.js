9326186b676fa4633437b4866a2eb60fb0320eb0
Courtney Brousseau <courtney.brousseau@berkeley.edu>
2017-10-23 16:24:40 -0700

    Tried to add radio buttons

--- js/form.js
+++ js/form.js
@@ -6,48 +6,45 @@ angular.module('form', [])
       $scope.wantsMediCal = false;
       $scope.wantsCalWORKS = false;
 
-        $scope.isCitizen = false;
-        $scope.income;
-        $scope.assets;
-        $scope.numberInHousehold;
+      $scope.isCitizen = false;
+      $scope.income;
+      $scope.assets;
+      $scope.numberInHousehold;
 
-        $scope.isStudent;
-        $scope.hasSSI;
-        $scope.numberOnSSI;
+      $scope.isStudent;
+      $scope.hasSSI;
+      $scope.numberOnSSI;
 
-        $scope.has60 = false;
-        $scope.hasDisabled = false;
+      $scope.has60 = false;
+      $scope.hasDisabled = false;
+      
+      $scope.isPregnant;
 
-        $scope.isNotPregnant = false;
-        $scope.isPregnant = false;
+      $scope.hasChildUnder19 = false;
 
-        $scope.hasChildUnder19 = false;
+      $scope.unavailableParent = false;
+      $scope.notUnavailableParent = false;
+      $scope.lowIncomeParent = false;
 
-        $scope.unavailableParent = false;
-        $scope.notUnavailableParent = false;
-        $scope.lowIncomeParent = false;
+      $scope.isCalFreshEligible = function () { 
+          return $scope.income <= 1915 + 670 * ($scope.numberInHousehold - 1) 
+          && !($scope.isStudent == "yes" && $scope.numberInHousehold == 1) 
+          && !($scope.numberInHousehold == $scope.numberOnSSI) 
+          && !($scope.numberInHousehold == 1 && $scope.hasSSI == "yes")
+          && $scope.isCitizen; 
+      };
 
-        $scope.isCalFreshEligible = function () { 
+      $scope.maxWorksIncome = function (x) { return 414 + 318 * x - 8 * x ^ 2}
 
-            return $scope.income <= 1915 + 670 * ($scope.numberInHousehold - 1) 
-            && !($scope.isStudent == "yes" && $scope.numberInHousehold == 1) 
-            && !($scope.numberInHousehold == $scope.numberOnSSI) 
-            && !($scope.numberInHousehold == 1 && $scope.hasSSI == "yes")
-            && $scope.isCitizen; 
-        };
+      $scope.isCalWORKSEligible = function () {
+          return $scope.meetsChildReqs()
+          && $scope.isCitizen
+          && $scope.income <= (420 + 320 * $scope.numberInHousehold - 8 * ($scope.numberInHousehold) ^ 2)
+          && ($scope.assets <= 2250 || (($scope.has60 || $scope.hasDisabled) && ($scope.assets <= 3250 )));
+      };
 
-        $scope.maxWorksIncome = function (x) { return 414 + 318 * x - 8 * x ^ 2}
-
-
-        $scope.isCalWORKSEligible = function () {
-            return $scope.meetsChildReqs()
-            && $scope.isCitizen
-            && $scope.income <= (420 + 320 * $scope.numberInHousehold - 8 * ($scope.numberInHousehold) ^ 2)
-            && ($scope.assets <= 2250 || (($scope.has60 || $scope.hasDisabled) && ($scope.assets <= 3250 )));
-        };
-
-        $scope.checkSSINumber = function () { return $scope.numberInHousehold > 1 && $scope.hasSSI == "yes"; };
-        $scope.meetsChildReqs = function () { return $scope.lowIncomeParent || $scope.unavailableParent || $scope.isPregnant}
+      $scope.checkSSINumber = function () { return $scope.numberInHousehold > 1 && $scope.hasSSI == "yes"; };
+      $scope.meetsChildReqs = function () { return $scope.lowIncomeParent || $scope.unavailableParent || $scope.isPregnant}
     
   }]);
 })(window.angular);
